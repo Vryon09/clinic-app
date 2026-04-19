0f05ab3c56dd7e54e1209be2217369619018726a
Vryon Antonio <vryonantonio@gmail.com>
2026-04-19 19:40:44 +0800

    feat: added skeleton loading

--- frontend/src/ui/components/PaginationBar.tsx
+++ frontend/src/ui/components/PaginationBar.tsx
@@ -6,72 +6,88 @@ import {
 } from "./shadcn/pagination";
 import { Button } from "./shadcn/button";
 import type { IPagination } from "@/types/Pagination";
+import { Skeleton } from "./shadcn/skeleton";
 
 function PaginationBar({
   itemName = "Item",
   paginationData,
   setPage,
+  isLoading,
 }: {
   itemName: string;
   paginationData: IPagination;
   setPage: React.Dispatch<React.SetStateAction<number>>;
+  isLoading: boolean;
 }) {
-  const from = !paginationData.pages
-    ? 0
-    : (paginationData.page - 1) * paginationData.limit + 1;
-  const to = Math.min(
-    paginationData.page * paginationData.limit,
-    paginationData.total,
-  );
+  const from = isLoading
+    ? null
+    : !paginationData.pages
+      ? 0
+      : (paginationData.page - 1) * paginationData.limit + 1;
+  const to = isLoading
+    ? null
+    : Math.min(
+        paginationData.page * paginationData.limit,
+        paginationData.total,
+      );
   return (
     <div className="mt-4 flex w-full items-center justify-between border-t border-neutral-300 px-2 py-2">
-      <p className="text-sm">
-        {itemName} {from === to ? from : `${from} - ${to}`} of{" "}
-        {paginationData.total}
-      </p>
+      {isLoading ? (
+        <>
+          <Skeleton className="h-4 w-24 bg-neutral-300" />
+          <Skeleton className="h-4 w-24 bg-neutral-300" />
+        </>
+      ) : (
+        <>
+          <p className="text-sm">
+            {itemName} {from === to ? from : `${from} - ${to}`} of{" "}
+            {paginationData.total}
+          </p>
 
-      <div className="w-fit">
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <Button
-                onClick={() => {
-                  if (paginationData.page === 1) return;
-                  setPage((prev) => prev - 1);
-                }}
-                disabled={paginationData.page === 1}
-                className="cursor-pointer"
-                variant="ghost"
-                size="icon-sm"
-              >
-                <ChevronsLeft />
-              </Button>
-            </PaginationItem>
-            <p className="text-sm">{paginationData.page}</p>
-            <PaginationItem>
-              <Button
-                onClick={() => {
-                  if (
-                    paginationData.page === paginationData.pages ||
-                    !paginationData.pages
-                  )
-                    return;
-                  setPage((prev) => prev + 1);
-                }}
-                disabled={
-                  paginationData.page === paginationData.pages ||
-                  !paginationData.pages
-                }
-                className="cursor-pointer"
-                variant="ghost"
-                size="icon-sm"
-              >
-                <ChevronsRight />
-              </Button>
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
-      </div>
+          <div className="w-fit">
+            <Pagination>
+              <PaginationContent>
+                <PaginationItem>
+                  <Button
+                    onClick={() => {
+                      if (paginationData.page === 1) return;
+                      setPage((prev) => prev - 1);
+                    }}
+                    disabled={paginationData.page === 1}
+                    className="cursor-pointer"
+                    variant="ghost"
+                    size="icon-sm"
+                  >
+                    <ChevronsLeft />
+                  </Button>
+                </PaginationItem>
+                <p className="text-sm">{paginationData.page}</p>
+                <PaginationItem>
+                  <Button
+                    onClick={() => {
+                      if (
+                        paginationData.page === paginationData.pages ||
+                        !paginationData.pages
+                      )
+                        return;
+                      setPage((prev) => prev + 1);
+                    }}
+                    disabled={
+                      paginationData.page === paginationData.pages ||
+                      !paginationData.pages
+                    }
+                    className="cursor-pointer"
+                    variant="ghost"
+                    size="icon-sm"
+                  >
+                    <ChevronsRight />
+                  </Button>
+                </PaginationItem>
+              </PaginationContent>
+            </Pagination>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- frontend/src/ui/components/pages/patients/PatientCard.tsx
+++ frontend/src/ui/components/pages/patients/PatientCard.tsx
@@ -2,48 +2,80 @@ import type { IPatient } from "@/types/PatientType";
 import { Card } from "../../shadcn/card";
 import dayjs from "dayjs";
 import type { ReactNode } from "react";
+import { Skeleton } from "../../shadcn/skeleton";
 
 function PatientCard({
   patient,
   children,
+  isPatientPending,
 }: {
   patient: IPatient;
   children?: ReactNode;
+  isPatientPending: boolean;
 }) {
   return (
     <Card className="flex h-fit min-w-86 flex-col gap-2 px-4 py-4">
-      <div>
-        <p className="text-lg font-semibold">
-          {`${patient?.firstName}${patient?.middleName ? ` ${patient?.middleName.slice(0, 1)}.` : ""} ${patient?.lastName}`}
-        </p>
+      {isPatientPending ? (
+        <div>
+          <Skeleton className="h-4.5 bg-neutral-300" />
 
-        <div className="mt-6 space-y-4">
-          <div className="grid grid-cols-2">
-            <p className="text-neutral-500">Birthday</p>
-            <p>{dayjs(patient?.dateOfBirth).format("MMMM DD, YYYY")}</p>
-          </div>
-
-          <div className="grid grid-cols-2">
-            <p className="text-neutral-500">Age</p>
-            <p>{dayjs().diff(dayjs(patient?.dateOfBirth), "year")}</p>
-          </div>
-
-          <div className="grid grid-cols-2">
-            <p className="text-neutral-500">Sex</p>
-            <p className="capitalize">{patient?.sex.toLowerCase()}</p>
-          </div>
-
-          <div className="grid grid-cols-2">
-            <p className="text-neutral-500">Phone</p>
-            <p> {patient?.phone}</p>
-          </div>
-
-          <div className="grid grid-cols-2">
-            <p className="text-neutral-500">Address</p>
-            <p>{patient?.address}</p>
+          <div className="mt-6 space-y-4">
+            <div className="grid grid-cols-2">
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+            </div>
+            <div className="grid grid-cols-2">
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+            </div>
+            <div className="grid grid-cols-2">
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+            </div>
+            <div className="grid grid-cols-2">
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+            </div>
+            <div className="grid grid-cols-2">
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+              <Skeleton className="h-4 w-24 bg-neutral-300" />
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <p className="text-lg font-semibold">
+            {`${patient?.firstName}${patient?.middleName ? ` ${patient?.middleName.slice(0, 1)}.` : ""} ${patient?.lastName}`}
+          </p>
+
+          <div className="mt-6 space-y-4">
+            <div className="grid grid-cols-2">
+              <p className="text-neutral-500">Birthday</p>
+              <p>{dayjs(patient?.dateOfBirth).format("MMMM DD, YYYY")}</p>
+            </div>
+
+            <div className="grid grid-cols-2">
+              <p className="text-neutral-500">Age</p>
+              <p>{dayjs().diff(dayjs(patient?.dateOfBirth), "year")}</p>
+            </div>
+
+            <div className="grid grid-cols-2">
+              <p className="text-neutral-500">Sex</p>
+              <p className="capitalize">{patient?.sex.toLowerCase()}</p>
+            </div>
+
+            <div className="grid grid-cols-2">
+              <p className="text-neutral-500">Phone</p>
+              <p> {patient?.phone}</p>
+            </div>
+
+            <div className="grid grid-cols-2">
+              <p className="text-neutral-500">Address</p>
+              <p>{patient?.address}</p>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="flex flex-1 items-end justify-end">{children}</div>
     </Card>
