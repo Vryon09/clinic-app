8d71bd307ab8be3b7553d924f4818661d4ef9942
Vryon Antonio <vryonantonio@gmail.com>
2026-04-21 21:17:42 +0800

    feat: validate lab result schema file upload

--- backend/src/routes/labResultsRoutes.ts
+++ backend/src/routes/labResultsRoutes.ts
@@ -5,11 +5,18 @@ import {
   getLabResults,
   uploadLabResult,
 } from "../controller/labResultsController";
+import { validateSchema } from "../middleware/validateSchema";
+import { uploadLabResultSchema } from "../schemas/labResultSchema";
 
 const router = Router();
 
 router.get("/:patientId", getLabResults);
-router.post("/upload", upload.single("file"), uploadLabResult);
+router.post(
+  "/upload",
+  upload.single("file"),
+  validateSchema(uploadLabResultSchema),
+  uploadLabResult,
+);
 router.delete("/:id", deleteLabResult);
 
 export default router;
--- backend/src/schemas/labResultSchema.ts
+++ backend/src/schemas/labResultSchema.ts
@@ -2,5 +2,4 @@ import z from "zod";
 
 export const uploadLabResultSchema = z.object({
   patientId: z.uuid(),
-  file: z.file(),
 });
